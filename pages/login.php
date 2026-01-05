b48c5e6ed17cf3f53d0130943e62055ba704646b
Raydacillo <laurencedacillo6@gmail.com>
2026-01-05 09:04:44 +0800

    new changes

---
+++ pages/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="">
+        <Label for="username">Username</Label>
+        <input type="text" id="username" class="username" name="username">
+        <Label for="username">Password</Label>
+        <input type="text" id="password" class="password" name="password">
+        <button type="submit">login</button>
+</body>
+</html>
