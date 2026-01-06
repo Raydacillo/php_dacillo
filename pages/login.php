dfe3e1f12fb9c0938e530058f2326e79664f7b98
Raydacillo <laurencedacillo6@gmail.com>
2026-01-06 08:08:45 +0800

    new changes

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <h1>Login</h1>
@@ -14,5 +14,8 @@
         <Label for="username">Password</Label>
         <input type="text" id="password" class="password" name="password">
         <button type="submit">login</button>
+
+        <?php
+        echo("hello world"); ?>
 </body>
 </html>
